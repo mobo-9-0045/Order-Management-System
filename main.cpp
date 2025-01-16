78a4850703281be5619810d8dcfbba3f035bdb75
mobo-9-0045 <abdelilahoman@gmail.com>
2025-01-15 22:50:52 -0500

    added routes with hardcode data and exemple dto with a minimal conception for project starter

--- main.cpp
+++ main.cpp
@@ -14,6 +14,14 @@ int main(){
         return "Cancel an order";
     });
 
+    CROW_ROUTE(app, "/get-positions")([](){
+        // i should get subaccouns first i just hardcode it from here that's it
+        std::string get_postions_api = "https://test.deribit.com/api/v2/private/get_positions?currency=BTC&kind=future&subaccount_id=64910";
+        auto response = cpr::Get(cpr::Url{get_postions_api});
+        printf("response: %d\n", response.status_code);
+        return response.text;
+    });
+
     CROW_ROUTE(app, "/place-order")([](){
         std::string get_oreder_time_api = "https://www.deribit.com/api/v2/public/get_time";
         auto response = cpr::Get(cpr::Url{get_oreder_time_api});
--- time.py
+++
@@ -1,8 +0,0 @@
-import time
-
-# Get the current time in milliseconds
-current_epoch_ms = int(time.time() * 1000)
-
-print(current_epoch_ms)
-
-
